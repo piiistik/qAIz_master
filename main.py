8f3ec2b0393f682ede3286d4d0af67596d057558
piiistik <stevkovasak@gmail.com>
2024-11-07 10:17:06 +0100

    commit

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import pytesseract
 from PIL import Image
+from screenshooter import screenshot
+import keyboard
 
-# Path to the Tesseract executable (required if not in your PATH)
-# Uncomment and update the path if Tesseract isn't in your PATH.
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+def read_screenshot():
+    screenshot()
 
-# Load the image file
-image_path = "obr\\test1.png"
-image = Image.open(image_path)
+    # Path to the Tesseract executable (required if not in your PATH)
+    # Uncomment and update the path if Tesseract isn't in your PATH.
 
-# Perform OCR on the image
-text = pytesseract.image_to_string(image, lang="eng", config="--psm 6")
+    pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-print("Extracted Text:")
-print(text)
+    # Load the image file
+    image_path = "screenshots\\screenshot.png"
+    image = Image.open(image_path)
+
+    # Perform OCR on the image
+    text = pytesseract.image_to_string(image, lang="eng", config="--psm 6")
+
+    print("Extracted Text:")
+    print(text)
+
+
+while True:
+    if keyboard.is_pressed("a") and keyboard.is_pressed("s"):
+        read_screenshot()
