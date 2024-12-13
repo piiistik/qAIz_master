5382ffbc332e6c04344c6a0bda6232c45b235519
smilinko <martin.smilnak@gmail.com>
2024-12-13 21:42:53 +0100

    auto mode and pretty log prints

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import keyboard
 import pyautogui
 from prompts import get_ai_response
 from json import dumps
-import webbrowser
-from time import sleep
+from time import time
 
 
-webbrowser.open('https://www.endquiz.com/quiz.php')
+PRINT = True
 
 
 def read_screenshot():
@@ -26,10 +25,10 @@ def read_screenshot():
 
     # Perform OCR on the image
     text = pytesseract.image_to_string(image, lang="eng", config="--psm 6")
+    print("Processed text: {text}\n\n")
 
-    prompt = "I will give you text from a quiz website. I want you to find the first question in it and identify it's answers. Select the correct one and return only it's text, no extra words.\n\nUse only words that are in the provided text. Give only raw answer, no extra words.\n###\n"
-    answer = get_ai_response(prompt + text)
-    print(answer)
+    prompt = "I will give you text from a quiz website. I want you to find the first question in it and identify it's answers. Select the correct one and return only it's text, no extra words.\n\nUse only words that are in the provided text. Give only raw answer, no extra words.\n\n###\n\n"
+    answer = get_ai_response(prompt + text, PRINT)
     find_text_coords(image_path, answer)
 
 def all_indexes(data, word):
@@ -40,10 +39,10 @@ def all_indexes(data, word):
     return indexes
 
 def find_match(sentence, data):
-    prompt = f"I will give you a list of pairs (int, string) and I want you to find the index of the first word of the sentence \"{sentence}\" when looking only on second elements in pairs. The strings can be faulty and might not match the sentence exactly, find the best possible match.\n\nReturn only raw index, no extra text."
+    prompt = f"I will give you a list of pairs (int, string) and I want you to find the index of the first word of the sentence \"{sentence}\" when looking only at second elements in pairs. The strings can be faulty and might not match the sentence exactly, find the best possible match.\n\nReturn only raw index, no extra text.\n\n###\n\n"
     
     prompt += dumps(list(enumerate(data)))
-    answer = get_ai_response(prompt)
+    answer = get_ai_response(prompt, PRINT)
 
     return int(answer.strip())
 
@@ -54,7 +53,6 @@ def find_text_coords(image_path, search_text):
 
     # Perform OCR on the image to get detailed data
     data = pytesseract.image_to_data(image, lang="eng", output_type=pytesseract.Output.DICT)
-    print(data)
     data_string = dumps(data["text"])
     index = find_match(search_text, data["text"])
     (x, y, w, h) = (data['left'][index], data['top'][index], data['width'][index], data['height'][index])
@@ -65,11 +63,21 @@ def click(x, y):
     pyautogui.click(x, y)
 
 
+auto = False
+last_time = time()
 while True:
-    if keyboard.is_pressed("a+i"):
+    if keyboard.is_pressed("shift"):
         read_screenshot()
-    #sleep(4)
-    #read_screenshot()
+
+    if auto and last_time + 4.7 < time():
+        last_time = time()
+        read_screenshot()
 
     if keyboard.is_pressed("c"):
         break
+
+    if keyboard.is_pressed("a"):
+        auto = True
+
+    if keyboard.is_pressed("m"):
+        auto = False
--- prompts.py
+++ prompts.py
@@ -4,11 +4,16 @@ import os
 client = openai.Client(api_key=os.getenv("OPENAI_API_KEY"))
 
 
-def get_ai_response(prompt):
+def get_ai_response(prompt, should_print):
+    if should_print:
+        print(f"Sending prompt: {prompt}\n")
     completion = client.chat.completions.create(
         model="gpt-4-turbo",
         messages=[
             {"role": "user", "content": prompt}
         ]
     )
-    return completion.choices[0].message.content
+    answer = completion.choices[0].message.content
+    if should_print:
+        print(f"Received answer: {answer}\n\n")
+    return answer
